9f71cb4f27ad7d0aff1d2544d4dc1bdf3801bf52
Vaibhav  Nimesh <98076349+vaibhavnimesh1@users.noreply.github.com>
2024-07-28 12:15:56 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,2 +1,3 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:5000");
+const URL = process.env.NODE_ENV === "production" ? "http://localhost:5000" : "https://sketch-book-server-rdwb.onrender.com"
+export const socket = io(URL);
